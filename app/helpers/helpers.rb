d78e30f794b04c3a1d3c4113d3668dda3343dad1
Roberto Livi <robertomlivi@outlook.com>
2020-04-11 17:02:26 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'pry'
 class Helpers
+  
+  def self.current_user(session)
+    binding.pry
+    user = User.find_by(session[:user_id])
+    user
+  end
+  
+  def self.is_logged_in?
+  end
+
 end
