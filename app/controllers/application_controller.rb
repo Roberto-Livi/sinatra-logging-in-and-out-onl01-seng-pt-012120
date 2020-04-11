4ccce0de9e2cf011a9724ff208f9df0573330597
Roberto Livi <robertomlivi@outlook.com>
2020-04-11 17:23:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(params[:username], params[:password])
     if user
       session[:user_id] = user.id
-      redirect :'/views/account'
+      redirect :'/account'
     end
     erb :error
   end
